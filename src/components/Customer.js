985e9dc138167b8304d68bc2d80ba7a38b6770d5
jonghwi Wang <wangjh789@daum.net>
2020-08-28 17:37:07 +0900

    apply material table design

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,12 +1,21 @@
 import React from "react";
+import TableRow from "@material-ui/core/TableRow"
+import TableCell from "@material-ui/core/TableCell"
+
 
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name} img={this.props.img} />
-                <CustomerInfo birth={this.props.birth} gender={this.props.gender} job={this.props.job} />
-            </div>
+
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.img} alt="profile/"></img></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birth}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+
         );
 
     }

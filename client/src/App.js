7088a4fb06ee6c8f939fc520dde4702cd4cf3c80
jonghwi Wang <wangjh789@daum.net>
2020-08-29 17:57:35 +0900

    add customer delete

--- client/src/App.js
+++ client/src/App.js
@@ -78,6 +78,7 @@ class App extends React.Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -86,6 +87,7 @@ class App extends React.Component {
                   this.state.customers.map(c => {
                     return (
                       <Customer
+                        stateRefresh={this.stateRefresh}
                         key={c.id}
                         id={c.id}
                         image={c.image}

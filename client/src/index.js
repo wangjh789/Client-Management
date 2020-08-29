a32cfb6dc52114fd493682bdf9638e6e5907f8ca
jonghwi Wang <wangjh789@daum.net>
2020-08-29 19:14:31 +0900

    add KR font

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, creatMuiTheme, createMuiTheme } from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR","serif"',
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MuiThemeProvider theme={theme}><App /></MuiThemeProvider>,
   document.getElementById('root')
 );
 
